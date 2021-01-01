9ddb298e661c9af1862e1b0e74f6204f977dc08b
zakuro <z@kuro.red>
2021-01-01 13:24:33 +0900

    Add ci cleancode

--- main.v
+++ main.v
@@ -5,7 +5,20 @@ import os
 import v.vmod
 
 fn setup_app(mut app Command) {
-	app.add_commands([])
+	mut ci := Command{
+		name: 'ci'
+		description: 'Run ci tests'
+	}
+	ci.add_commands([
+		Command{
+			name: 'cleancode'
+			description: 'test-cleancode'
+			execute: fn (cmd Command) ? {
+				exit(os.system('VCOLORS=always v test-cleancode'))
+			}
+		},
+	])
+	app.add_commands([ci])
 }
 
 fn main() {
