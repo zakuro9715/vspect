027a14b5bb50f53b2a1a4156ed2ea2fe68274d81
zakuro <z@kuro.red>
2021-01-13 19:10:57 +0900

    scanner.each_token

--- inspect/scanner/scanner.v
+++ inspect/scanner/scanner.v
@@ -2,9 +2,23 @@ module scanner
 
 import v.pref { Preferences }
 import v.scanner
+import v.token
+
+import cli { Command }
 
 type Scanner = scanner.Scanner
 
 pub fn new(path string, prefs &pref.Preferences) &Scanner {
 	return scanner.new_scanner_file(path, .parse_comments, prefs)
+}
+
+type TokenFn = fn(token.Token)?
+pub fn (mut s Scanner) each_token(f TokenFn) ? {
+	for {
+		t := s.scan()
+		f(t) ?
+		if t.kind == .eof {
+			return
+		}
+	}
 }
