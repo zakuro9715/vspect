59611758ec0db51b84e16eedcc730a0ce1a62413
zakuro <z@kuro.red>
2021-01-17 08:15:04 +0900

    Remove unused

--- inspect/scanner/scanner.v
+++ inspect/scanner/scanner.v
@@ -11,18 +11,6 @@ pub fn new(path string, prefs &pref.Preferences) &Scanner {
 	return scanner.new_scanner_file(path, .parse_comments, prefs)
 }
 
-type TokenFn = fn (token.Token) ?
-
-pub fn (mut s Scanner) each_token(f TokenFn) ? {
-	for {
-		t := s.scan()
-		f(t) ?
-		if t.kind == .eof {
-			return
-		}
-	}
-}
-
 pub fn inspect_tokens(paths []string, prefs &pref.Preferences) {
 	for path in paths {
 		mut scanner := new(path, prefs)
