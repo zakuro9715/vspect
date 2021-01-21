275e69268df478c3d5233fcc2885f8f26ee5f657
zakuro <z@kuro.red>
2021-01-22 00:09:53 +0900

    [inspect/ast] add exprs

--- inspect/ast/expr.v
+++ inspect/ast/expr.v
@@ -2,6 +2,19 @@ module ast
 
 import v.ast
 
+pub fn (mut b StringBuilder) exprs(exprs ...ast.Expr) {
+	if exprs.len == 0 {
+		b.writeln('[]')
+		return
+	}
+	b.begin_array()
+	for expr in exprs {
+		b.expr(expr)
+		b.insert_array_comma()
+	}
+	b.end_array()
+}
+
 pub fn (mut b StringBuilder) expr(expr ast.Expr) {
 	match expr {
 		else { b.writeln(expr) }
