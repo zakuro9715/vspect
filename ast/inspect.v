f22dfb88b450bf505cceec39e4e34197167d20b2
zakuro <z@kuro.red>
2021-01-23 19:14:50 +0900

    [ast] clean

--- ast/inspect.v
+++ ast/inspect.v
@@ -1,6 +1,61 @@
 module ast
 
-import v.ast
+import strings
+import v.ast { Scope }
+import v.ast.walker
+import v.parser
+import v.pref
+import v.table
+
+// V 0.2.1 71d3d4c
+pub struct InspectOpts {
+	func string
+}
+
+pub fn inspect(paths []string, prefs &pref.Preferences, opts &InspectOpts) {
+	global_scope := Scope{
+		parent: 0
+	}
+	for path in paths {
+		table := table.new_table()
+		f := parser.parse_file(path, table, .parse_comments, prefs, &global_scope)
+		mut b := Inspector{
+			table: table
+			target_fn: opts.func
+		}
+		if b.target_fn.len > 0 {
+			walker.walk(b, f)
+		} else {
+			b.file(&f)
+		}
+		print(b.str())
+	}
+}
+
+struct Pos {
+pub mut:
+	i            int
+	line         int
+	is_line_head bool = true
+}
+
+pub fn (mut p Pos) inc_line() {
+	p.line++
+	p.is_line_head = true
+}
+
+pub struct Inspector {
+	target_fn string
+mut:
+	pos      Pos
+	table    &table.Table
+	buf      strings.Builder
+	indent_n int
+}
+
+pub fn (mut b Inspector) str() string {
+	return b.buf.str()
+}
 
 fn (mut b Inspector) write<T>(v T) {
 	if b.pos.is_line_head {
