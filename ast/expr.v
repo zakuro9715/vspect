23e6c3aa4ca77940e05c072321b59bf80eb0cc52
zakuro <z@kuro.red>
2021-01-23 21:53:30 +0900

    [ast] infix_expr

--- ast/expr.v
+++ ast/expr.v
@@ -26,7 +26,7 @@ import v.ast
 	IfExpr
 	IfGuardExpr
 	IndexExpr
-	InfixExpr
+o	InfixExpr
 	IntegerLiteral
 	Likely
 	LockExpr
@@ -61,6 +61,23 @@ pub fn (mut b Inspector) exprs(exprs ...ast.Expr) {
 
 pub fn (mut b Inspector) expr(expr ast.Expr) {
 	match expr {
+		ast.InfixExpr { b.infix_expr(expr) }
 		else { b.writeln(expr) }
 	}
+}
+
+pub fn (mut b Inspector) infix_expr(expr ast.InfixExpr) {
+	b.begin_struct('InfixExpr')
+
+	b.write_field('op', expr.op)
+	b.write_field('pos', expr.pos)
+	b.write_label('left')
+	b.expr(expr.left)
+	b.write_label('right')
+	b.expr(expr.right)
+	b.write_field('auto_locked', expr.auto_locked)
+	b.write_label('or_block')
+	b.expr(expr.or_block)
+
+	b.end_struct()
 }
