1435585d869ed29ec67f8368a63af941929c6355
zakuro <z@kuro.red>
2021-01-26 19:46:40 +0900

    call_expr

--- ast/expr.v
+++ ast/expr.v
@@ -11,7 +11,7 @@ import v.ast
 	AtExpr
 	BoolLiteral
 	CTempVar
-	CallExpr
+o	CallExpr
 	CastExpr
 	ChanInit
 	CharLiteral
@@ -69,6 +69,7 @@ pub fn (mut b Inspector) expr(expr ast.Expr) {
 		return
 	}
 	match expr {
+		ast.CallExpr { b.call_expr(expr) }
 		ast.Ident { b.ident(expr) }
 		ast.InfixExpr { b.infix_expr(expr) }
 		ast.OrExpr { b.or_expr(expr) }
@@ -77,6 +78,68 @@ pub fn (mut b Inspector) expr(expr ast.Expr) {
 		ast.PrefixExpr { b.prefix_expr(expr) }
 		else { b.writeln(expr) }
 	}
+}
+
+// TODO: share type
+fn (mut b Inspector) call_arg(arg ast.CallArg) {
+	b.begin_struct('CallArg')
+
+	b.write_field('pos', arg.pos)
+	b.write_field('is_mut', arg.is_mut)
+	b.write_field('is_tmp_autofree', arg.is_tmp_autofree)
+	b.write_field('share_type', arg.share)
+	b.write_label('expr')
+	b.expr(arg.expr)
+	b.write_label('comments')
+	b.exprs(...arg.comments)
+	b.write_label('type')
+	b.typ(arg.typ)
+
+	b.end_struct()
+}
+
+// TODO: scope
+pub fn (mut b Inspector) call_expr(expr ast.CallExpr) {
+	b.begin_struct('CallExpr')
+
+	b.write_field('language', expr.language)
+	b.write_field('mod', expr.mod)
+	b.write_field('name', expr.name)
+	b.write_field('pos', expr.pos)
+	b.write_field('is_field', expr.is_field)
+	b.write_field('is_method', expr.is_method)
+	b.write_field('free_receiver', expr.free_receiver)
+	b.write_field('should_be_skipped', expr.should_be_skipped)
+	b.write_label('left')
+	b.expr(expr.left)
+	b.write_label('left_type')
+	b.typ(expr.left_type)
+	b.write_label('receiver_type')
+	b.typ(expr.receiver_type)
+	b.write_label('return_type')
+	b.typ(expr.return_type)
+	b.write_label('from_embed_type')
+	b.typ(expr.from_embed_type)
+
+	b.write_label('args')
+	b.begin_array()
+	for arg in expr.args {
+		b.call_arg(arg)
+		b.array_comma()
+	}
+	b.end_array()
+
+	b.write_label('expected_arg_types')
+	b.types(...expr.expected_arg_types)
+	b.write_label('generic_types')
+	b.types(...expr.generic_types)
+	b.write_field('generic_list_pos', expr.generic_list_pos)
+	b.write_label('or_block')
+	b.or_expr(expr.or_block)
+	b.write_label('comments')
+	b.exprs(...expr.comments)
+
+	b.end_struct()
 }
 
 pub fn (mut b Inspector) ident(expr ast.Ident) {
