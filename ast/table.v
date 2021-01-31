04d5eb410d325720fb124122528af0ce5949f95d
zakuro <z@kuro.red>
2021-01-31 18:29:09 +0900

    cleanup

--- ast/table.v
+++ ast/table.v
@@ -41,9 +41,9 @@ pub fn (mut b Inspector) param(v table.Param) {
 	b.write_any_field('name', v.name)
 	b.write_any_field('is_mut', v.is_mut)
 	b.write_any_field('is_hidden', v.is_hidden)
-	b.write_any_field('pos', v.pos)
+	b.write_pos_field('', v.pos)
 	b.write_type_field('typ', v.typ)
-	b.write_any_field('type_pos', v.type_pos)
+	b.write_pos_field('type_pos', v.type_pos)
 
 	b.end_struct()
 }
